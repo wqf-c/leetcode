dd5e54b714eb16d45da0c000396b2404fd9945ca
wqf <284660487@qq.com>
2020-07-20 12:32:15 +0800

    .

---
+++ src/Solution91to135/Solution120.java
@@ -0,0 +1,55 @@
+package Solution91to135;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution120 {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int size = triangle.size();
+        if(size == 0) return 0;
+        int[][] dp = new int[size][size];
+        dp[0][0] = triangle.get(0).get(0);
+        for(int i = 1; i < size; ++i){
+            for(int j = 0; j <= i; ++j){
+                if(j == 0){
+
+                    dp[i][j] = triangle.get(i).get(j) + dp[i - 1][j];
+                }else if(j == i){
+                    dp[i][j] = triangle.get(i).get(j) + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle.get(i).get(j);
+                }
+            }
+        }
+        int min = dp[size - 1][0];
+        for(int i = 1; i < size; ++i){
+            if (dp[size - 1][i] < min) min = dp[size - 1][i];
+        }
+        return min;
+    }
+
+    public static void main(String[] args){
+        Solution120 s120 = new Solution120();
+        List<List<Integer>> lists = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(2);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(4);
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(6);
+        list3.add(5);
+        list3.add(7);
+        List<Integer> list4 = new ArrayList<>();
+        list4.add(4);
+        list4.add(1);
+        list4.add(8);
+        list4.add(3);
+        lists.add(list1);
+        lists.add(list2);
+        lists.add(list3);
+        lists.add(list4);
+        System.out.println(s120.minimumTotal(lists));
+    }
+}
